3d5d24b45810b79e00ffd3e938aff63b51954253
Mayank Kapoor <mayank.kapoor1203@gmail.com>
2024-02-01 14:04:37 +0530

    changes in spellings and loop count

--- enduser/src/main/java/com/enduser/KafkaConfig.java
+++ enduser/src/main/java/com/enduser/KafkaConfig.java
@@ -10,7 +10,7 @@ public class KafkaConfig {
     @KafkaListener(topics = AppConstants.LOCATION_UPDATE_TOPIC, groupId = AppConstants.GROUP_ID)
     public void updatedLocation(String value) {
 
-
+    	
         System.out.println(value);
 
     }
